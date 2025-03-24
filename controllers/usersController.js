ea56c2b5e6dd01870b38f8a7d70897a5ce839e87
Fred <leferpz@gmail.com>
2025-03-24 12:19:11 -0400

    Added usersStorage

---
+++ controllers/usersController.js
@@ -0,0 +1,19 @@
+const usersStorage = require('../storages/usersStorage');
+
+exports.usersListGet = (req, res) => {
+    res.render("index", {
+        title: "User list",
+        users: usersStorage.getUsers()
+    })
+}
+
+exports.usersCreateGet = (req, res) => {
+    res.render("createUser", { title: "New User Form" })
+}
+
+exports.usersCreatePost = (req, res) => {
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({firstName, lastName});
+    res.redirect("/")
+}
+
