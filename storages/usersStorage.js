2ea4d97f3e95e0bf0297612e2337b6be3a65afdc
Fred <leferpz@gmail.com>
2025-03-24 15:15:33 -0400

    Added fields to update user

--- storages/usersStorage.js
+++ storages/usersStorage.js
@@ -6,7 +6,7 @@ class UsersStorage {
       this.id = 0;
     }
   
-    addUser({ firstName, lastName }) {
+    addUser({ firstName, lastName, email, age, bio }) {
       const id = this.id;
       this.storage[id] = { id, firstName, lastName, email, age, bio };
       this.id++;
